5084f2d514949fb2101df00e71aa68ee9bc73c06
amarlad <34013724+amarlad@users.noreply.github.com>
2017-12-31 09:01:40 -0800

    Add files via upload

--- AirportData/AirportData.py
+++ AirportData/AirportData.py
@@ -31,8 +31,8 @@ def sql_insert(to_db):
 if __name__ == "__main__":
     create_table()
     
-    for i in range (1, 13):
-        filename = path + csvfiles + str(i) + '.csv'
+    for months in range (1, 13):
+        filename = path + csvfiles + str(months) + '.csv'
         with open (filename, 'r') as csvfile:
             # csv.DictReader uses first line in file for column headings by default
             # comma is default delimiter
@@ -40,4 +40,5 @@ if __name__ == "__main__":
             for i in dr:
                 to_db = [(i['YEAR'], i['MONTH'] , i['DAY_OF_MONTH'] , i['DAY_OF_WEEK'] , i['FL_DATE'] , i['UNIQUE_CARRIER'] , i['AIRLINE_ID'] , i['CARRIER'] , i['ORIGIN_AIRPORT_ID'] , i['ORIGIN_AIRPORT_SEQ_ID'] , i['ORIGIN_CITY_MARKET_ID'] , i['ORIGIN'] , i['ORIGIN_CITY_NAME'] , i['ORIGIN_STATE_ABR'] , i['ORIGIN_STATE_FIPS'] , i['ORIGIN_STATE_NM'] , i['ORIGIN_WAC'] , i['DEST_AIRPORT_ID'] , i['DEST_AIRPORT_SEQ_ID'] , i['DEST_CITY_MARKET_ID'] , i['DEST'] , i['DEST_CITY_NAME'] , i['DEST_STATE_ABR'] , i['DEST_STATE_FIPS'] , i['DEST_STATE_NM'] , i['DEST_WAC'] , i['CRS_DEP_TIME'] , i['DEP_TIME'] , i['DEP_DELAY'] , i['DEP_DELAY_NEW'] , i['CRS_ARR_TIME'] , i['ARR_TIME'] , i['ARR_DELAY'] , i['ARR_DELAY_NEW'] , i['CANCELLED'] , i['CANCELLATION_CODE'] , i['DIVERTED'] , i['CRS_ELAPSED_TIME'] , i['ACTUAL_ELAPSED_TIME'] , i['AIR_TIME'] , i['DISTANCE'] , i['CARRIER_DELAY'] , i['WEATHER_DELAY'] , i['NAS_DELAY'] , i['SECURITY_DELAY'] , i['LATE_AIRCRAFT_DELAY']) for i in dr]
             sql_insert(to_db)
+            print('completed' + str(months))
 
